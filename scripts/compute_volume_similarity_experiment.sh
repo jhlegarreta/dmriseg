961e2346f4b9caa5a7d3eea1dee8e0b6086e0753
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2024-07-17 17:30:42 -0400

    ENH: Adopt more meaningful volume similarity definition
    Adopt a more meaningful volume similarity definition:
    ```
    VS = 1 - \frac{\abs{V_{pred} − V_{gdth}}}{V_{pred} + V_{gdth}}
    ```
    
    Make the necessary changes to the performance computation scripts.
    
    Add some additional scripts that compute only the volume similarity and
    which were used to check its results, and to compute the volume
    similarity on the predictions instead of re-computing all performance
    measures over again.

---
+++ scripts/compute_volume_similarity_experiment.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+# Source the virtualenvwrapper.sh script
+source $(which virtualenvwrapper.sh)
+
+workon dmriseg
+
+contrast=$1
+
+# Folds
+folds=("fold-0" "fold-1" "fold-2" "fold-3" "fold-4")
+
+# Build the labels for the i/o dirs/files
+if [[ ${contrast} == "t1" ]]; then
+  contrast_folder_label=dmri_hcp_t1
+elif [[ ${contrast} == "t2" ]]; then
+  contrast_folder_label=dmri_hcp_t2
+elif [[ ${contrast} == "b0" ]]; then
+  contrast_folder_label=dmri_hcp_b0
+elif [[ ${contrast} == "dwi" ]]; then
+  contrast_folder_label=dmri_hcp_sphm_b1000-2000-3000
+elif [[ ${contrast} == "dwi1k" ]]; then
+  contrast_folder_label=dmri_hcp_sphm_b1000
+elif [[ ${contrast} == "dwi2k" ]]; then
+  contrast_folder_label=dmri_hcp_sphm_b2000
+elif [[ ${contrast} == "dwi3k" ]]; then
+  contrast_folder_label=dmri_hcp_sphm_b3000
+elif [[ ${contrast} == "fa" ]]; then
+  contrast_folder_label=dmri_hcp_fa
+elif [[ ${contrast} == "md" ]]; then
+  contrast_folder_label=dmri_hcp_md
+elif [[ ${contrast} == "rd" ]]; then
+  contrast_folder_label=dmri_hcp_rd
+elif [[ ${contrast} == "evalse1" ]]; then
+  contrast_folder_label=dmri_hcp_evals_e1
+elif [[ ${contrast} == "evalse2" ]]; then
+  contrast_folder_label=dmri_hcp_evals_e2
+elif [[ ${contrast} == "evalse3" ]]; then
+  contrast_folder_label=dmri_hcp_evals_e3
+elif [[ ${contrast} == "ak" ]]; then
+  contrast_folder_label=dmri_hcp_ak
+elif [[ ${contrast} == "mk" ]]; then
+  contrast_folder_label=dmri_hcp_mk
+elif [[ ${contrast} == "rk" ]]; then
+  contrast_folder_label=dmri_hcp_rk
+else
+  echo "Contrast not available:" ${contrast}
+  echo "Aborting."
+  exit 0
+fi
+
+test_split_label=test
+
+data_root_dirname=/mnt/data/cerebellum_parc/experiments_minimal_pipeline
+contrast_dirname=${data_root_dirname}/${contrast_folder_label}
+
+folds_root_dirname=/mnt/data/connectome/participant_folds
+
+gnd_th_labelmap_root_dirname=/mnt/data/cerebellum_parc/experiments_minimal_pipeline/labelmaps
+
+in_labels_fname=/mnt/data/lut/suit_diedrichsen_lut0255_nuclei_colored.tsv
+
+perf_script_dirname=/home/jhlegarreta/src/dmriseg/scripts
+
+echo "Starting volume similarity computation..."
+for fold in "${folds[@]}"; do
+
+  echo ${fold}
+
+  fold_dirname=${contrast_dirname}/${fold}
+
+  in_participants_fname=${folds_root_dirname}/${fold}/cerebellum_participants_hcp_qc_no_sub_prefix_${fold}_split-${test_split_label}.tsv
+  in_gnd_th_labelmap_dirname=${gnd_th_labelmap_root_dirname}/${fold}/test_set
+  in_pred_labelmap_dirname=${contrast_dirname}/${fold}/results/prediction
+
+  perf_dirname=${fold_dirname}/results/performance
+
+  # Rename existing vs files to sitk_vs_
+  old_sitk_vs_fname=${perf_dirname}/vs.tsv
+  new_sitk_vs_fname=${perf_dirname}/sitk_vs.tsv
+  if [ -e "${old_sitk_vs_fname}" ]; then
+      mv "${old_sitk_vs_fname}" "${new_sitk_vs_fname}"
+      echo "File renamed to ${new_sitk_vs_fname}"
+  else
+      echo "File ${old_sitk_vs_fname} does not exist"
+  fi
+
+  old_sitk_vs_fname=${perf_dirname}/vs_stats.tsv
+  new_sitk_vs_fname=${perf_dirname}/sitk_vs_stats.tsv
+  if [ -e "${old_sitk_vs_fname}" ]; then
+      mv "${old_sitk_vs_fname}" "${new_sitk_vs_fname}"
+      echo "File renamed to ${new_sitk_vs_fname}"
+  else
+      echo "File ${old_sitk_vs_fname} does not exist"
+  fi
+
+  python ${perf_script_dirname}/compute_volume_similarity.py \
+    ${in_participants_fname} \
+    ${in_gnd_th_labelmap_dirname} \
+    ${in_pred_labelmap_dirname} \
+    ${in_labels_fname} \
+    ${perf_dirname}
+
+done
+
+echo "Finished"
